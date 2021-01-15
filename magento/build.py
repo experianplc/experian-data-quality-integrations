86d7319c5bf43bb9e4a259b1c4c170f9d084d45b
Tabeth Nkangoh <tabeth@tabeth.com>
2021-01-15 16:27:43 -0500

    Test configuration updates

--- magento/build.py
+++ magento/build.py
@@ -10,3 +10,6 @@ files = set(ts_files) - set(d_ts_files)
 for file in files:
     newFile = file.replace("src", "lib").replace(".ts", ".js")
     os.system(f"npx webpack --config config.js --output {newFile} {file}")
+    # Update the first line for coverage purposes
+    newLine = "(function(e, a) { /* istanbul ignore next */ for(var i in a) e[i] = a[i]; }(this, (/* istanbul ignore next */ function(modules) { // webpackBootstrap"
+    os.system(f"sed -i '1s/.*/{re.escape(newLine)}/' {newFile}")
