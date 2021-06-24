c2607fc42b05251ac6ac08e78056c28f7fb72043
Tabeth Nkangoh <tabeth@tabeth.com>
2021-06-24 16:28:37 -0400

    Update integration code

--- ccb/src/2.7/root/cm/ext_personCorrespInfoPage/integration.ts
+++ ccb/src/2.7/root/cm/ext_personCorrespInfoPage/integration.ts
@@ -17,12 +17,8 @@ let interval = setInterval(function() {
     }
 
     window.EdqConfig = Object.assign(parent.window.EdqConfig, {
-      PRO_WEB_SUBMIT_TRIGGERS: [
-        {
-          type: "click",
-          element: trigger
-        },
-      ],
+      GLOBAL_INTUITIVE_NO_SAVED_TARGET: true,
+      NO_SAVED_TARGET: true,
       PRO_WEB_COUNTRY: "USA",
       PRO_WEB_CALLBACK: function(savedTarget, newEvent) {
         (parent as any).doClose();
@@ -30,150 +26,167 @@ let interval = setInterval(function() {
       }
     });
 
-    const mailingInfoSection: HTMLElement = document.querySelector("#secRow_2 table");
-    const seasonInfoSection: HTMLElement  = document.querySelector("#secRow_4 table");
+    // Add EDQ Validate button for mailing address
+    (function() {
+      let el = document.createElement("input");
+      el.className = "oraButton uiMargin";
+      el.tabIndex = 5;
+      el.type = "button";
+      el.id = "edq-validate-mailing-address";
+      el.value = "EDQ Validate";
+      el.title = "EDQ Validate";
+      document.getElementById("C1_VAL_ADDR_SW").parentElement.appendChild(el);
+    })();
 
-    const proWebUseTypedown = parent.window.EdqConfig.PRO_WEB_USE_TYPEDOWN ||
-      currentElement.getAttribute("PRO_WEB_USE_TYPEDOWN");
+    const mailingAddressConfig: any = Object.assign({
+      PRO_WEB_SUBMIT_TRIGGERS: [
+        {
+          "type": "click",
+          "element": document.getElementById("edq-validate-mailing-address")
+        }
+      ],
+      GLOBAL_INTUITIVE_ELEMENT: document.getElementById("ADDRESS1"),
+      GLOBAL_INTUITIVE_MAPPING: [
+        { field: document.getElementById("ADDRESS1"), elements: ["address.addressLine1"] },
+        { field: document.getElementById("ADDRESS2"), elements: ["address.addressLine2"] },
+        { field: document.getElementById("CITY"), elements: ["address.locality"] },
+        { field: document.getElementById("STATE"), elements: ["address.province"] },
+        { field: document.getElementById("POSTAL"), elements: ["address.postalCode"] },
+        { field: document.getElementById("COUNTY"), elements: ["components.county1"] },
+      ],
 
-    mailingInfoSection.onclick = function() {
-      window.EdqConfig = Object.assign(window.EdqConfig, {
-        GLOBAL_INTUITIVE_ELEMENT: document.getElementById("ADDRESS1"),
-        GLOBAL_INTUITIVE_MAPPING: [
-          { field: document.getElementById("ADDRESS1"), elements: ["address.addressLine1"] },
-          { field: document.getElementById("ADDRESS2"), elements: ["address.addressLine2"] },
-          { field: document.getElementById("CITY"), elements: ["address.locality"] },
-          { field: document.getElementById("STATE"), elements: ["address.province"] },
-          { field: document.getElementById("POSTAL"), elements: ["address.postalCode"] },
-          { field: document.getElementById("COUNTY"), elements: ["components.county1"] },
-        ],
+      PRO_WEB_MAPPING: [
+        {
+          field: document.getElementById('ADDRESS1'),
+          elements: ['Primary number', 'Street'],
+          separator: ' ',
+          typedownFieldSelector: '#typedown-final--address-line-one',
+          modalFieldSelector: '#interaction-address--original-address-line-one',
+        },
 
-        PRO_WEB_TYPEDOWN_TRIGGER: document.getElementById('ADDRESS1'),
-        PRO_WEB_MAPPING: [
-          {
-            field: document.getElementById('ADDRESS1'),
-            elements: ['Primary number', 'Street'],
-            separator: ' ',
-            typedownFieldSelector: '#typedown-final--address-line-one',
-            modalFieldSelector: '#interaction-address--original-address-line-one',
-          },
+        {
+          field: document.getElementById('ADDRESS2'),
+          elements: ['Secondary number'],
+          separator: '',
+          typedownFieldSelector: '#typedown-final--address-line-two',
+          modalFieldSelector: '#interaction-address--original-address-line-two',
+        },
 
-          {
-            field: document.getElementById('ADDRESS2'),
-            elements: ['Secondary number'],
-            separator: '',
-            typedownFieldSelector: '#typedown-final--address-line-two',
-            modalFieldSelector: '#interaction-address--original-address-line-two',
-          },
+        {
+          field: document.getElementById('CITY'),
+          elements: ['City name'],
+          separator: '',
+          typedownFieldSelector: '#typedown-final--city',
+          modalFieldSelector: '#interaction-address--original-locality',
+        },
 
-          {
-            field: document.getElementById('CITY'),
-            elements: ['City name'],
-            separator: '',
-            typedownFieldSelector: '#typedown-final--city',
-            modalFieldSelector: '#interaction-address--original-locality',
-          },
+        {
+          field: document.getElementById('STATE'),
+          elements: ['State code'],
+          separator: '',
+          typedownFieldSelector: '#typedown-final--state',
+          modalFieldSelector: '#interaction-address--original-province',
+        },
 
-          {
-            field: document.getElementById('STATE'),
-            elements: ['State code'],
-            separator: '',
-            typedownFieldSelector: '#typedown-final--state',
-            modalFieldSelector: '#interaction-address--original-province',
-          },
+        {
+          field: document.getElementById('POSTAL'),
+          elements: ['ZIP Code', '+4 code'],
+          separator: '-',
+          typedownFieldSelector: '#typedown-final--postal-code',
+          modalFieldSelector: '#interaction-address--original-postal-code',
+        },
 
-          {
-            field: document.getElementById('POSTAL'),
-            elements: ['ZIP Code', '+4 code'],
-            separator: '-',
-            typedownFieldSelector: '#typedown-final--postal-code',
-            modalFieldSelector: '#interaction-address--original-postal-code',
-          },
+        {
+          field: document.getElementById('COUNTY'),
+          elements: ['County name'],
+          separator: '',
+          typedownFieldSelector: '#typedown-final--county',
+          modalFieldSelector: '#interaction-address--original-postal-code'
+        },
+      ]
+    }, window.EdqConfig);
 
-          {
-            field: document.getElementById('COUNTY'),
-            elements: ['County name'],
-            separator: '',
-            typedownFieldSelector: '#typedown-final--county',
-            modalFieldSelector: '#',
-          },
-        ]
-      });
 
-      if (proWebUseTypedown) {
-        document.getElementById("ADDRESS1").onclick = window.EDQ.address.proWeb.typedownEventListener;
-      }
-    };
+    // Add EDQ Validate button for mailing address
+    (function() {
+      let el = document.createElement("input");
+      el.className = "oraButton uiMargin";
+      el.tabIndex = 5;
+      el.type = "button";
+      el.id = "edq-validate-seasonal-address";
+      el.value = "EDQ Validate";
+      el.title = "EDQ Validate";
+      document.getElementById("SEAS_ADDR$C1_VAL_ADDR_SW").parentElement.appendChild(el);
+    })();
 
-    seasonInfoSection.onclick = function() {
-      window.EdqConfig = Object.assign(window.EdqConfig, {
-        GLOBAL_INTUITIVE_ELEMENT: document.getElementById("SEAS_ADDR$ADDRESS1"),
-        GLOBAL_INTUITIVE_MAPPING: [
-          { field: document.getElementById("SEAS_ADDR$ADDRESS1"), elements: ["address.addressLine1"] },
-          { field: document.getElementById("SEAS_ADDR$ADDRESS2"), elements: ["address.addressLine2"] },
-          { field: document.getElementById("SEAS_ADDR$CITY"), elements: ["address.locality"] },
-          { field: document.getElementById("SEAS_ADDR$STATE"), elements: ["address.province"] },
-          { field: document.getElementById("SEAS_ADDR$POSTAL"), elements: ["address.postalCode"] },
-          { field: document.getElementById("SEAS_ADDR$COUNTY"), elements: ["components.county1"] }
-        ],
+    const seasonalInfoConfig = Object.assign({
+      PRO_WEB_SUBMIT_TRIGGERS: [
+        {
+          "type": "click",
+          "element": document.getElementById("edq-validate-seasonal-address")
+        }
+      ],
+      GLOBAL_INTUITIVE_ELEMENT: document.getElementById("SEAS_ADDR$ADDRESS1"),
+      GLOBAL_INTUITIVE_MAPPING: [
+        { field: document.getElementById("SEAS_ADDR$ADDRESS1"), elements: ["address.addressLine1"] },
+        { field: document.getElementById("SEAS_ADDR$ADDRESS2"), elements: ["address.addressLine2"] },
+        { field: document.getElementById("SEAS_ADDR$CITY"), elements: ["address.locality"] },
+        { field: document.getElementById("SEAS_ADDR$STATE"), elements: ["address.province"] },
+        { field: document.getElementById("SEAS_ADDR$POSTAL"), elements: ["address.postalCode"] },
+        { field: document.getElementById("SEAS_ADDR$COUNTY"), elements: ["components.county1"] }
+      ],
 
-        PRO_WEB_TYPEDOWN_TRIGGER: document.getElementById('SEAS_ADDR$ADDRESS1'),
-        PRO_WEB_MAPPING: [
-          {
-            field: document.getElementById('SEAS_ADDR$ADDRESS1'),
-            elements: ['Primary number', 'Street'],
-            separator: ' ',
-            typedownFieldSelector: '#typedown-final--address-line-one',
-            modalFieldSelector: '#interaction-address--original-address-line-one',
-          },
+      PRO_WEB_MAPPING: [
+        {
+          field: document.getElementById('SEAS_ADDR$ADDRESS1'),
+          elements: ['Primary number', 'Street'],
+          separator: ' ',
+          typedownFieldSelector: '#typedown-final--address-line-one',
+          modalFieldSelector: '#interaction-address--original-address-line-one',
+        },
 
-          {
-            field: document.getElementById('SEAS_ADDR$ADDRESS2'),
-            elements: ['Secondary number'],
-            separator: '',
-            typedownFieldSelector: '#typedown-final--address-line-two',
-            modalFieldSelector: '#interaction-address--original-address-line-two',
-          },
+        {
+          field: document.getElementById('SEAS_ADDR$ADDRESS2'),
+          elements: ['Secondary number'],
+          separator: '',
+          typedownFieldSelector: '#typedown-final--address-line-two',
+          modalFieldSelector: '#interaction-address--original-address-line-two',
+        },
 
-          {
-            field: document.getElementById('SEAS_ADDR$CITY'),
-            elements: ['City name'],
-            separator: '',
-            typedownFieldSelector: '#typedown-final--city',
-            modalFieldSelector: '#interaction-address--original-locality',
-          },
+        {
+          field: document.getElementById('SEAS_ADDR$CITY'),
+          elements: ['City name'],
+          separator: '',
+          typedownFieldSelector: '#typedown-final--city',
+          modalFieldSelector: '#interaction-address--original-locality',
+        },
 
-          {
-            field: document.getElementById('SEAS_ADDR$STATE'),
-            elements: ['State code'],
-            separator: '',
-            typedownFieldSelector: '#typedown-final--state',
-            modalFieldSelector: '#interaction-address--original-province',
-          },
+        {
+          field: document.getElementById('SEAS_ADDR$STATE'),
+          elements: ['State code'],
+          separator: '',
+          typedownFieldSelector: '#typedown-final--state',
+          modalFieldSelector: '#interaction-address--original-province',
+        },
 
-          {
-            field: document.getElementById('SEAS_ADDR$POSTAL'),
-            elements: ['ZIP Code', "+4 code"],
-            separator: '-',
-            typedownFieldSelector: '#typedown-final--postal-code',
-            modalFieldSelector: '#interaction-address--original-postal-code',
-          },
+        {
+          field: document.getElementById('SEAS_ADDR$POSTAL'),
+          elements: ['ZIP Code', "+4 code"],
+          separator: '-',
+          typedownFieldSelector: '#typedown-final--postal-code',
+          modalFieldSelector: '#interaction-address--original-postal-code',
+        },
 
-          {
-            field: document.getElementById('SEAS_ADDR$COUNTY'),
-            elements: ['County name'],
-            separator: ' ',
-            typedownFieldSelector: '#typedown-final--county',
-            modalFieldSelector: '#interaction-address--original-postal-code',
-          },
+        {
+          field: document.getElementById('SEAS_ADDR$COUNTY'),
+          elements: ['County name'],
+          separator: ' ',
+          typedownFieldSelector: '#typedown-final--county',
+          modalFieldSelector: '#interaction-address--original-postal-code',
+        },
 
-        ]
-      });
-
-      if (proWebUseTypedown) {
-        document.getElementById("SEAS_ADDR$ADDRESS1").onclick = window.EDQ.address.proWeb.typedownEventListener;
-      }
-    }
+      ]
+    }, window.EdqConfig);
 
     const globalIntuitiveAuthToken =
       parent.window.EdqConfig.GLOBAL_INTUITIVE_AUTH_TOKEN ||
@@ -218,10 +231,8 @@ let interval = setInterval(function() {
         PRO_WEB_AUTH_TOKEN: parent.window.EdqConfig.PRO_WEB_AUTH_TOKEN,
         SOAP_ACTION_URL: parent.window.EdqConfig.SOAP_ACTION_URL,
         GLOBAL_INTUITIVE_AUTH_TOKEN: parent.window.EdqConfig.GLOBAL_INTUITIVE_AUTH_TOKEN,
-        PRO_WEB_USE_TYPEDOWN: parent.window.EdqConfig.PRO_WEB_USE_TYPEDOWN,
-        PRO_WEB_STYLESHEET: parent.window.EdqConfig.PRO_WEB_STYLESHEET,
-        PRO_WEB_VERIFICATION_URL: "http://localhost:8002/verification-unicorn.js",
-        PRO_WEB_TYPEDOWN_URL: parent.window.EdqConfig.PRO_WEB_TYPEDOWN_URL,
+        PRO_WEB_STYLESHEET_URL: parent.window.EdqConfig.PRO_WEB_STYLESHEET_URL,
+        PRO_WEB_VERIFICATION_URL: parent.window.EdqConfig.PRO_WEB_VERIFICATION_URL,
         PRO_WEB_EDQ_URL: parent.window.EdqConfig.PRO_WEB_EDQ_URL,
         GLOBAL_INTUITIVE_URL: parent.window.EdqConfig.GLOBAL_INTUITIVE_URL
       },
@@ -231,6 +242,12 @@ let interval = setInterval(function() {
           // the integration has been loaded into. So in this case
           // each page would indicate the URL where it was created in
           // and load its own data into that particular spot
+
+          let verificationMailing: any = new window.VerificationUnicorn(mailingAddressConfig);
+          verificationMailing.activateValidation();
+
+          let verificationSeasonal: any = new window.VerificationUnicorn(seasonalInfoConfig);
+          verificationSeasonal.activateValidation();
 
           if (!parent.window.edqLoaded) {
             parent.window.edqLoaded = {};
@@ -248,16 +265,13 @@ let interval = setInterval(function() {
         },
 
         globalIntuitive: function() {
-          window.EDQ.address.globalIntuitive.activateValidation(
-            document.getElementById("ADDRESS1")
-          );
+          let globalIntuitiveMailing: any = new window.GlobalIntuitiveUnicorn(mailingAddressConfig);
+          globalIntuitiveMailing.activateValidation();
 
-          window.EDQ.address.globalIntuitive.activateValidation(
-            document.getElementById("SEAS_ADDR$ADDRESS1")
-          );
+          let globalIntuitiveSeasonal: any = new window.GlobalIntuitiveUnicorn(seasonalInfoConfig);
+          globalIntuitiveSeasonal.activateValidation();
         }
       }
     }); 
   }
 }, 1000);
-
